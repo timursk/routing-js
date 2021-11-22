bfac65cd415f7a4da44e7184752beec718125da9
Timur Salikhov <cruzemen@mail.ru>
2021-11-23 02:12:33 +0300

    feat: finish art-quiz task

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Pictures } from './pages/Pictures';
 import { PicturesGame } from './pages/PicturesGame';
 
 import Utils from './utils/Utils';
+import images from './utils/images.json';
 
 const homeInstance = new Home();
 const settingsInstance = new Settings();
@@ -17,6 +18,12 @@ const artistsGameInstance = new ArtistsGame();
 const picturesInstance = new Pictures();
 const PicturesGameInstance = new PicturesGame();
 
+async function loadJSON() {
+    const jsonStr = JSON.stringify(images);
+    const json = JSON.parse(jsonStr);
+    return json;
+}
+let imagesInfo = await loadJSON();
 
 
 const routes = {
@@ -26,27 +33,44 @@ const routes = {
     '/pictures': picturesInstance,
     '/artists/game': artistsGameInstance,
     '/pictures/game': PicturesGameInstance,
+    'ERROR': `alert('ERROR')`,
 }
 document.querySelector('header').innerHTML = await homeInstance.render();
 const router = async () => {
-    // const header = null || document.querySelector('header');
-    // const main = null || document.querySelector('main');
-    // const footer = null || document.querySelector('footer');
-
     const request = Utils.parseURL();
-    const parsedURL = (request.resource ? `/${request.resource}` : '/') + (request.id ? `/${request.id}` : '');
-    console.log(parsedURL);
+    const parsedURL = (request.resource ? `/${request.resource}` : '/') + (request.id ? `/game` : '');
     const page = routes[parsedURL] ? routes[parsedURL] : 'ERROR';
-    console.log(page);
     document.body.innerHTML = await page.render();
     
+    // game render
     if (request.id != undefined) {
-        const test = new Utils.Game(request.id); 
-        test.render();
+        if (request.resource == 'artists') {
+            const game = new Utils.Game(request.id, imagesInfo); 
+            Utils.Game.result = 0;
+            await game.render();
+        } else if (request.resource == 'pictures') {
+            const game = new Utils.GamePictures(request.id, imagesInfo); 
+            Utils.GamePictures.result = 0;
+            await game.render();
+        }
+    }
+    // settings render
+    if (parsedURL == '/settings') {
+        const settings = new Utils.Settings();
+        settings.renderLocalStorage();
+        settings.addEvents();
     }
 
-}
+    //artists record
+    if (parsedURL == '/artists') {
+        Utils.setRecordsArtists();
+    }
 
+    //pictures record 
+    if (parsedURL == '/pictures') {
+        Utils.setRecordsPictures();
+    }
+}
 
 window.addEventListener('hashchange', router);
 window.addEventListener('load', router);
