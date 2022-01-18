bd2802793921d0ea15bd85b30b254a6bd3f0a6f5
timursk <timur.dev@mail.ru>
2022-01-18 17:15:53 +0300

    feat: add eslint&prettier&ts config

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+var world = "world";
+var wor = '123';
+wor = 22;
+function hello(world) {
+    if (world === void 0) { world = "world"; }
+    return "Hello ".concat(world, "! ");
+}
+exports.hello = hello;
+console.log('2');
